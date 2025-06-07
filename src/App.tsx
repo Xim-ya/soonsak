51a28fcbd03c5d4fabb177fce71947628b8cf8fd
XimYa <hj_lee_com@naver.com>
2025-06-07 23:39:03 +0900

    chore : 프로젝트 폴더 구조 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
@@ -18,4 +17,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+}); 
