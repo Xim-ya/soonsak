21d4ef1fbf8e709924701fa6596fc3d743efd2cd
XimYa <hj_lee_com@naver.com>
2025-06-14 17:45:41 +0900

    chore : font, js lint 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -6,7 +7,7 @@ import StackNavigator from './shared/navigation/StackNavigator';
 import TabNavigator from './shared/navigation/TabNavigator';
 import '@/shared/exntensions/arrayExtension';
 
-// React Query Client 생성
+// React Query Client 생성 (컴포넌트 밖에서 생성)
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -19,8 +20,21 @@ const queryClient = new QueryClient({
 });
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Pretendard-Bold': require('../assets/fonts/Pretendard-Bold.otf'),
+    'Pretendard-SemiBold': require('../assets/fonts/Pretendard-SemiBold.otf'),
+    'Pretendard-Medium': require('../assets/fonts/Pretendard-Medium.otf'),
+    'Pretendard-Regular': require('../assets/fonts/Pretendard-Regular.otf'),
+    'staatliches_regular': require('../assets/fonts/Staatliches-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <></>;
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
+      <StatusBar style="light" />
       <NavigationContainer>
         <TabNavigator />
       </NavigationContainer>
