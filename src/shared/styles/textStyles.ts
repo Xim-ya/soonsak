b09af9c6cd46573b067ca83c4cba0a3f0e26331b
XimYa <hj_lee_com@naver.com>
2025-07-30 22:00:51 +0900

    feature : textStyle 기본 색상 적용

--- src/shared/styles/textStyles.ts
+++ src/shared/styles/textStyles.ts
@@ -1,6 +1,7 @@
 // src/shared/styles/appTextStyle.ts
 
 import { TextStyle } from 'react-native';
+import colors from './colors';
 
 const pretendard = {
   bold: 'Pretendard-Bold',
@@ -36,112 +37,131 @@ const appTextStyle: AppTextStyles = {
   extraFont: {
     fontFamily: pretendard.staatliches,
     fontSize: 24,
+    color: colors.white,
   },
   web1: {
     fontFamily: pretendard.bold,
     fontSize: 40,
     lineHeight: 52,
     letterSpacing: -0.2,
+    color: colors.white,
   },
   web2: {
     fontFamily: pretendard.bold,
     fontSize: 36,
     lineHeight: 46,
     letterSpacing: -0.2,
+    color: colors.white,
   },
   web3: {
     fontFamily: pretendard.bold,
     fontSize: 32,
     lineHeight: 48,
     letterSpacing: -0.2,
+    color: colors.white,
   },
   highlight: {
     fontFamily: pretendard.bold,
     fontSize: 32,
     lineHeight: 36,
     letterSpacing: -0.2,
+    color: colors.white,
   },
   headline3: {
     fontFamily: pretendard.semiBold,
     fontSize: 18,
     lineHeight: 22,
     letterSpacing: -0.2,
+    color: colors.white,
   },
   headline2: {
     fontFamily: pretendard.bold,
     fontSize: 20,
     lineHeight: 30,
     letterSpacing: -0.2,
+    color: colors.white,
   },
   headline1: {
     fontFamily: pretendard.bold,
     fontSize: 24,
     lineHeight: 37,
     letterSpacing: -0.2,
+    color: colors.white,
   },
   title3: {
     fontFamily: pretendard.bold,
     fontSize: 15,
     lineHeight: 22,
     letterSpacing: -0.2,
+    color: colors.white,
   },
   title3Bold: {
     fontFamily: pretendard.bold,
     fontSize: 14,
     lineHeight: 20,
+    color: colors.white,
   },
   title2: {
     fontFamily: pretendard.semiBold,
     fontSize: 16,
     lineHeight: 22,
     letterSpacing: -0.2,
+    color: colors.white,
   },
   title1: {
     fontFamily: pretendard.bold,
     fontSize: 16,
     lineHeight: 22,
     letterSpacing: -0.2,
+    color: colors.white,
   },
   body3: {
     fontFamily: pretendard.medium,
     fontSize: 14,
     lineHeight: 20,
     letterSpacing: -0.2,
+    color: colors.white,
   },
   body2: {
     fontFamily: pretendard.medium,
     fontSize: 15,
     lineHeight: 22,
     letterSpacing: -0.2,
+    color: colors.white,
   },
   body1: {
     fontFamily: pretendard.semiBold,
     fontSize: 15,
     lineHeight: 22,
     letterSpacing: -0.2,
+    color: colors.white,
   },
   alert1: {
     fontFamily: pretendard.semiBold,
     fontSize: 13,
     lineHeight: 18,
     letterSpacing: -0.2,
+    color: colors.white,
   },
   alert2: {
     fontFamily: pretendard.regular,
     fontSize: 13,
     lineHeight: 18,
     letterSpacing: -0.2,
+    color: colors.white,
   },
   desc: {
     fontFamily: pretendard.regular,
     fontSize: 12,
     lineHeight: 14,
     letterSpacing: -0.2,
+    color: colors.white,
   },
   nav: {
     fontFamily: pretendard.medium,
     fontSize: 12,
     letterSpacing: -0.2,
+    color: colors.white,
   },
 };
 
